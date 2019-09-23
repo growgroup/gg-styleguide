8c65b8cb0442f70f823b88a80ccc67b8f10ddbda
ishihara takashi <ishihara@grow-group.jp>
2019-09-23 22:57:05 +0900

    Create components-manager.js

---
+++ components-manager.js
@@ -0,0 +1,55 @@
+const spawn = require('child_process').spawn;
+const process = require("process");
+
+// 引数から取得
+const component_id = process.argv[2];
+
+const seckey = "QWqhFhbahkXvAeF%40HE8";
+const endpoint = "https://grow-group.jp/designsystem/";
+
+if (!component_id) {
+  console.log("コンポーネントIDの指定は必ず行ってください。");
+}
+
+const markupDownload = spawn("wget", ["--content-disposition", "-P", "app/inc/components", endpoint + "?component_id=" + component_id + "&download=true&key=" + seckey + "&type=markup"]);
+markupDownload.stderr.on("data", data => {
+  console.log(data.toString());
+});
+markupDownload.stdout.on("data", data => {
+  console.log(data.toString());
+});
+markupDownload.on("close", code => {
+  if (code !== 0) {
+    console.error(`マークアップファイルのダウンロードに失敗しました : ${code}`)
+  }
+  console.log('マークアップデータファイルのダウンロードに成功しました。')
+});
+
+const styleDownload = spawn("wget", ["--content-disposition", "-P", "app/assets/scss/object/components", endpoint + "?component_id=" + component_id + "&download=true&key=" + seckey + "&type=style"]);
+
+styleDownload.stderr.on("data", data => {
+  console.log(data.toString());
+});
+styleDownload.stdout.on("data", data => {
+  console.log(data.toString());
+});
+styleDownload.on("close", code => {
+  if (code !== 0) {
+    console.error(`スタイルマークアップファイルのダウンロードに失敗しました : ${code}`);
+  }
+  console.log('スタイルデータファイルのダウンロードに成功しました。')
+});
+
+const javascriptDownload = spawn("wget", ["--content-disposition", "-P", "app/assets/js", endpoint + "?component_id=" + component_id + "&download=true&key=" + seckey + "&type=javascript"]);
+javascriptDownload.stderr.on("data", data => {
+  console.log(data.toString());
+});
+javascriptDownload.stdout.on("data", data => {
+  console.log(data.toString());
+});
+javascriptDownload.on("close", code => {
+  if (code !== 0) {
+    console.error(`JavaScriptマークアップファイルのダウンロードに失敗しました : ${code}`);
+  }
+  console.log('JavaScriptファイルのダウンロードに成功しました。')
+});
