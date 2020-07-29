caa0142b8e5fbb15beb8f6ca686d957a38ac4379
北濱大輔 <kitahama@kitahamadaisukenoMacBook-Pro.local>
2020-07-29 18:15:19 +0900

    JSファイルの既存/新規分割&&

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -10,10 +10,12 @@ import Tab from './app/tab.js';
 import CurrentNav from './app/current-nav.js';
 import buildFormat from './app/format.js';
 import OwlCarousel from 'owl.carousel';
+import modaal from 'modaal';
 
-import fontAwesome from "font-awesome/scss/font-awesome.scss"
-import OwlCss from "owl.carousel/dist/assets/owl.carousel.css"
-import OwlThemeCss from "owl.carousel/dist/assets/owl.theme.default.css"
+import fontAwesome from "font-awesome/scss/font-awesome.scss";
+import OwlCss from "owl.carousel/dist/assets/owl.carousel.css";
+import OwlThemeCss from "owl.carousel/dist/assets/owl.theme.default.css";
+import modaalCss from 'modaal/dist/css/modaal.css';
 
 class App {
   constructor() {
@@ -27,182 +29,113 @@ class App {
     this.CurrentNav = new CurrentNav();
     this.Tab = new Tab();
 
-      //SPメニューの高さ取得
+    //SPメニューの高さ取得
     function menuHight() {
-        var win = $(window).innerWidth();
-        if (win > 750) {
-            return false;
-        }
+      var win = $(window).innerWidth();
+      if (win > 750) {
+        return false;
+      }
 
-        var $smpHeaderHeight = $('.l-header').height();
-        var windowHeight = window.innerHeight;
-        var winH = windowHeight - $smpHeaderHeight;
+      var $smpHeaderHeight = $('.l-header').height();
+      var windowHeight = window.innerHeight;
+      var winH = windowHeight - $smpHeaderHeight;
 
-        console.log($smpHeaderHeight);
+      console.log($smpHeaderHeight);
 
-        //動かすターゲットを取得
-        var targetSlidebarWrap = $('.c-slidebar-menu'),
-            targetSlidebarMenu = $('.c-slidebar__parent'),
-            targetSlidebarBtn = $('.c-slidebar-menu__parent');
+      //動かすターゲットを取得
+      var targetSlidebarWrap = $('.c-slidebar-menu'),
+        targetSlidebarMenu = $('.c-slidebar__parent'),
+        targetSlidebarBtn = $('.c-slidebar-menu__parent');
 
 
-        //いざ実行(クリックしたら処理を実行する)
-        targetSlidebarBtn.on('click', function () {
-            $('.c-slidebar-menu').toggleClass('is-active');
+      //いざ実行(クリックしたら処理を実行する)
+      targetSlidebarBtn.on('click', function () {
+        $('.c-slidebar-menu').toggleClass('is-active');
 
-        });
+      });
     }
 
-      //フッターメニューSPスライド
-      function menuSlide() {
-          var win = $(window).innerWidth();
-          if (win > 750) {
-              return false;
-          }
-          $('.l-footer__block').on('click',function() {
-              $(this).children(".l-footer__menutitle").toggleClass('is-open');
-              $(this).children(".l-footer__menulist.is-sub").slideToggle();
-          })
+    //フッターメニューSPスライド
+    function menuSlide() {
+      var win = $(window).innerWidth();
+      if (win > 750) {
+        return false;
       }
-      //owlcarousel
-      function owlCarousel() {
-          var owls = $('.owl-carousel');
-          if (owls.length === 0) {
-              return false
-          }
-          //->スライダー
-          owls.imagesLoaded(function () {
-              $('.c-main-visual__slider').owlCarousel({
-                  items: 1,
-                  margin: 0,
-                  dots: true,
-                  loop: true,
-                  nav: false,
-                  autoplayHoverPause: true,
-                  autoplay: true,
-                  autoplaySpeed: 500,
-                  autoWidth: false,
-                  autoHeight: false,
-                  center: true,
-                  navText: ['<img src="../assets/images/icon-slider-prev.svg" />','<img src="../assets/images/icon-slider-next.svg" />'],
-              });
-          });
-          //->カルーセル
-          owls.imagesLoaded(function () {
-              $('.c-main-visual__carousel').owlCarousel({
-                  items: 1,
-                  margin: 16,
-                  dots: true,
-                  loop: true,
-                  nav: true,
-                  autoplayHoverPause: true,
-                  autoplay: false,
-                  autoplaySpeed: 500,
-                  autoWidth: false,
-                  autoHeight: false,
-                  center: true,
-                  navText: ['<div class="c-main-visual__nav is-left"><i class="fa fa-angle-left" aria-hidden="true"></i></div>','<div class="c-main-visual__nav is-right"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'],
-                  responsive : {
-                    // breakpoint from 0 up
-                    0 : {
-                      stagePadding: 32,
-                    },
-                    // breakpoint from 750  up
-                    750 : {
-                      stagePadding: 64,
-                    },
-                    // breakpoint from 950 up
-                    950 : {
-                      stagePadding: 156,
-                    },
-                    1400 : {
-                      stagePadding: 312,
-                    }
-                  }
-              });
-          });
-          //->カルーセル_ノーマル
-          owls.imagesLoaded(function () {
-          $('.c-main-visual__carousel.is-normal').owlCarousel({
-            items: 1,
-            margin: 8,
-            dots: true,
-            loop: true,
-            nav: true,
-            autoplayHoverPause: true,
-            autoplay: false,
-            autoplaySpeed: 500,
-            autoWidth: false,
-            autoHeight: false,
-            center: true,
-            navText: ['<div class="c-main-visual__nav is-left"><i class="fa fa-angle-left" aria-hidden="true"></i></div>','<div class="c-main-visual__nav is-right"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'],
-            responsive : {
-              // breakpoint from 0 up
-              0 : {
-                stagePadding: 32,
-              },
-              // breakpoint from 750  up
-              750 : {
-                stagePadding: 64,
-              },
-              // breakpoint from 950 up
-              950 : {
-                stagePadding: 156,
-              },
-              1400 : {
-                stagePadding: 312,
-              }
-            }
-          });
-        });
-          //->カルーセル_レイヤー
-          owls.imagesLoaded(function () {
-            $('.c-main-visual-layer__carousel').owlCarousel({
-              items: 1,
-              margin: 0,
-              dots: true,
-              loop: true,
-              nav: false,
-              autoplayHoverPause: true,
-              autoplay: true,
-              autoplaySpeed: 500,
-              autoWidth: false,
-              autoHeight: false,
-              center: true,
-              navText: ['<img src="../assets/images/icon-slider-prev.svg" />','<img src="../assets/images/icon-slider-next.svg" />'],
-            });
-          });
-          //->カード_カルーセル
-          owls.imagesLoaded(function () {
-              $('.js-card-slider').owlCarousel({
-                  margin: 0,
-                  dots: false,
-                  loop: true,
-                  nav: true,
-                  autoplayHoverPause: true,
-                  autoplay: true,
-                  autoplaySpeed: 500,
-                  autoWidth: false,
-                  autoHeight: false,
-                  center: true,
-                  navText: ['<img src="../assets/images/icon-slider-prev.svg" />','<img src="../assets/images/icon-slider-next.svg" />'],
-                  responsive : {
-                    // breakpoint from 0 up
-                    0 : {
-                      items: 1,
-                    },
-                    // breakpoint from 750 up
-                    750 : {
-                      items: 3,
-                    }
-                  }
-              });
-          });
-      }
+      $('.l-footer__block').on('click', function () {
+        $(this).children(".l-footer__menutitle").toggleClass('is-open');
+        $(this).children(".l-footer__menulist.is-sub").slideToggle();
+      })
+    }
 
-    $(function() {
-        menuSlide();
-        owlCarousel();
+    //owlcarousel
+    function owlCarousel() {
+      var owls = $('.owl-carousel');
+      if (owls.length === 0) {
+        return false
+      }
+      //->スライダー
+      owls.imagesLoaded(function () {
+        $('.c-main-visual__slider').owlCarousel({
+          items: 1,
+          margin: 0,
+          dots: true,
+          loop: true,
+          nav: false,
+          autoplayHoverPause: true,
+          autoplay: true,
+          autoplaySpeed: 500,
+          autoWidth: false,
+          autoHeight: false,
+          center: true,
+          navText: ['<img src="../assets/images/icon-slider-prev.svg" />', '<img src="../assets/images/icon-slider-next.svg" />'],
+        });
+      });
+      //->カード_カルーセル
+      owls.imagesLoaded(function () {
+        $('.js-card-slider').owlCarousel({
+          margin: 0,
+          dots: false,
+          loop: true,
+          nav: true,
+          autoplayHoverPause: true,
+          autoplay: true,
+          autoplaySpeed: 500,
+          autoWidth: false,
+          autoHeight: false,
+          center: true,
+          navText: ['<img src="../assets/images/icon-slider-prev.svg" />', '<img src="../assets/images/icon-slider-next.svg" />'],
+          responsive: {
+            // breakpoint from 0 up
+            0: {
+              items: 1,
+            },
+            // breakpoint from 750 up
+            750: {
+              items: 3,
+            }
+          }
+        });
+      });
+    }
+
+    // modaal
+    function modaal() {
+      // ====================
+      // <HTML>
+      // <a href="path/to/image.jpg" class="js-modal-image" data-modaal-desc="My Description">Show</a>
+      // or
+      // <button data-modaal-content-source="path/to/image.jpg" class="js-modal-image">Show</button>
+      // ====================
+
+      $('.js-modal-image').modaal({
+        type: 'image'
+      });
+    }
+
+    $(function () {
+      menuSlide();
+      owlCarousel();
+      modaal();
     });
   }
 }
