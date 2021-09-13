50bfa225fa5be987f7400e4ec74cd2b05ce4cf29
shunya-masuda09 <s.masuda@grow-group.jp>
2021-09-13 11:01:24 +0900

    改善DB

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -18,8 +18,42 @@
   growApp.prototype.myCode = function () {
   }
 
+  growApp.prototype.enterAnimation = function () {
+    let $mainVisual = $('.c-main-visual');
+    if (!$mainVisual.length === 0) {
+      return false;
+    }
+
+    function changeClass(el, className, time) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          el.addClass(className);
+          resolve(className);
+        }, time);
+      });
+    }
+
+    var loaderFunction = function () {
+      // ここから実行
+      if ($mainVisual) {
+        changeClass($mainVisual, "is-active", 1000).then(function () {
+
+        });
+      }
+    };
+
+    // first entry only
+    if (!sessionStorage.getItem('loading')) {
+      sessionStorage.setItem('loading', true);
+      loaderFunction();
+    } else {
+      $mainVisual.addClass('is-already');
+    }
+  };
+
   $(function () {
     var app = new growApp();
     app.myCode();
+    app.enterAnimation();
   });
 })(jQuery);
