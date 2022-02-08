a635f218b4ec129a125f5be35f150f4acafc3bf4
吉岡 寿輝 <toshiki-yoshioka@yoshiokanoMacBook-Pro.local>
2022-02-08 19:06:29 +0900

    app.jsの整理　gsapのdefault化　gallery-slider.scssのdefault化

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -11,47 +11,15 @@ import Tab from './app/tab.js';
 import CurrentNav from './app/current-nav.js';
 import buildFormat from './app/format.js';
 import myScript from './scripts.js';
-import OwlCarousel from 'owl.carousel';
+import OwlCarousel from "./app/owl-carousel.js";
+import SlickSlider from "./app/slick-slider";
 import modaal from 'modaal';
+import CustomFunctions from "./app/custom";
 import modal from './app/modal.js';
-import {scrollfire} from "./app/scrolltrigger.js";
+import GsapAnimation from "./app/gsap";
 import anime from 'animejs';
-
-
 import fontAwesome from "font-awesome/scss/font-awesome.scss";
-import OwlCss from "owl.carousel/dist/assets/owl.carousel.css";
-import OwlThemeCss from "owl.carousel/dist/assets/owl.theme.default.css";
 import modaalCss from 'modaal/dist/css/modaal.css';
-import slick from "slick-carousel/slick/slick.min.js";
-import slickCss from "slick-carousel/slick/slick.css";
-import slickTheme from "slick-carousel/slick/slick-theme.css";
-
-
-class ScrollReveal {
-  constructor(options){
-    this.options = options;
-  }
-
-  reveal(el, options, duration){
-    $(el).css({
-      visibility: "visible",
-      opacity: 0,
-    })
-    scrollfire( el, function(){
-      // console.log(el);
-      var args = {
-        targets: el,
-        // opacity: 1,
-        // translateY: -options.distance,
-        // duration: 600
-      }
-      args = Object.assign(options,args);
-      anime(args);
-    }, {
-      offset: window.innerHeight
-    })
-  }
-}
 
 class App {
   constructor() {
@@ -66,234 +34,14 @@ class App {
     this.Slidebar = new Slidebar();
     this.CurrentNav = new CurrentNav();
     this.Tab = new Tab();
-
-    //SPメニューの高さ取得
-    function menuHight() {
-      var win = $(window).innerWidth();
-      if (win > 750) {
-        return false;
-      }
-
-      var $smpHeaderHeight = $('.l-header').height();
-      var windowHeight = window.innerHeight;
-      var winH = windowHeight - $smpHeaderHeight;
-
-      console.log($smpHeaderHeight);
-
-      //動かすターゲットを取得
-      var targetSlidebarWrap = $('.c-slidebar-menu'),
-        targetSlidebarMenu = $('.c-slidebar__parent'),
-        targetSlidebarBtn = $('.c-slidebar-menu__parent');
-
-
-      //いざ実行(クリックしたら処理を実行する)
-      targetSlidebarBtn.on('click', function () {
-        $('.c-slidebar-menu').toggleClass('is-active');
-
-      });
+    this.gsap = new GsapAnimation();
+    this.defaultOptions = {
+      gsap: this.gsap,
+      utils: this.Utils
     }
-
-    //フッターメニューSPスライド
-    function menuSlide() {
-      var win = $(window).innerWidth();
-      if (win > 750) {
-        return false;
-      }
-      $('.l-footer__block').on('click', function () {
-        $(this).children(".l-footer__menutitle").toggleClass('is-open');
-        $(this).children(".l-footer__menulist.is-sub").slideToggle();
-      })
-    }
-
-
-    //owlcarousel
-    function owlCarousel() {
-      var owls = $('.owl-carousel');
-      if (owls.length === 0) {
-        return false
-      }
-      //->スライダー
-      owls.imagesLoaded(function () {
-        $('.c-main-visual__slider').owlCarousel({
-          items: 1,
-          margin: 0,
-          dots: true,
-          loop: true,
-          nav: false,
-          autoplayHoverPause: true,
-          autoplay: true,
-          autoplaySpeed: 500,
-          autoWidth: false,
-          autoHeight: false,
-          center: true,
-          animateOut: 'fadeOut',
-          navText: ['<img src="../assets/images/icon-slider-prev.svg" />', '<img src="../assets/images/icon-slider-next.svg" />'],
-        });
-      });
-      //->カード_カルーセル
-      owls.imagesLoaded(function () {
-        if ($('.js-card-slider').find('.c-card__block').length < 3){
-          $('.js-card-slider').toggleClass('owl-carousel');
-          return false;
-        }
-        $('.js-card-slider').owlCarousel({
-          margin: 0,
-          dots: false,
-          loop: true,
-          nav: true,
-          autoplayHoverPause: true,
-          autoplay: true,
-          autoplaySpeed: 500,
-          autoWidth: false,
-          autoHeight: false,
-          center: true,
-          navText: ['<img src="../assets/images/icon-slider-prev.svg" />', '<img src="../assets/images/icon-slider-next.svg" />'],
-          responsive: {
-            // breakpoint from 0 up
-            0: {
-              items: 1,
-            },
-            // breakpoint from 750 up
-            750: {
-              items: 3,
-            }
-          }
-        });
-      });
-    }
-    //- スクロールリーバル
-    function reveal() {
-      function domEach(items, callback) {
-        if (typeof items === "string") {
-          var items = $(items);
-        }
-        for (var i = 0; i < items.length; i++) {
-          var item = items[i];
-          // callback = callback.bind(item)
-          callback(i, item);
-        }
-      }
-      window.sr = new ScrollReveal({duration: 600, mobile: true});
-      var baseEasing = 'cubicBezier(0.175, 0.885, 0.32, 1.275)';
-      var baseDistance = '8';
-
-      sr.reveal(".c-main-visual,.l-page-header", {
-        duration: 1400,
-        opacity: 1,
-        //translateY: -baseDistance,
-        delay: 900,
-      }, 100);
-
-      sr.reveal(".c-solution__wrap  > *",{
-        scale: [0.9, 1],
-        opacity: 1,
-        duration: 600,
-        delay: anime.stagger(100),
-        translateX: [-baseDistance,0],
-        easing: baseEasing
-      });
-      domEach(".c-card-sm__block", function(key, item){
-
-        sr.reveal(item,{
-          scale: {
-            value: [0.9,1],
-            // easing: "linear"
-          },
-          opacity: 1,
-          duration: 600,
-          delay: 100*key,
-          translateY: [-baseDistance,0],
-          easing: baseEasing
-        } );
-      });
-    }
-
-
-
-    // slick
-    function slickSlider() {
-
-      if ($('.js-gallery-slider').length <0 ){
-        return false;
-      }
-
-      if ($('.c-gallery-slider__thumbnail .c-gallery-slider__thumbnail-image').length < 2) {
-        $(".c-gallery-slider__thumbnail").addClass("is-simple-none");
-      }
-
-      // 上段
-      $('.c-gallery-slider__main').slick({
-        infinite: true,
-        arrow: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<div class="slick-prev"><img src="../assets/images/icon-arrow-left.png" /></div>',
-        nextArrow: '<div class="slick-next"><img src="../assets/images/icon-arrow-next.png" /></div>',
-        asNavFor: '.c-gallery-slider__thumbnail',
-        autoplaySpeed: 2500,
-        // centerPadding: '23.28%',
-        centerMode: true,
-        variableWidth: true,
-        dots: false,
-        responsive: [{
-          breakpoint: 750,
-          settings: {
-            variableWidth: false,
-            autoplaySpeed: 2500,
-            slidesToShow: 1
-          }
-        }]
-      });
-
-      // 下段
-      $('.c-gallery-slider__thumbnail').slick({
-        accessibility: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        speed: 400,
-        arrows: false,
-        dots: false,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        asNavFor: '.c-gallery-slider__main',
-        focusOnSelect: true,
-        responsive: [{
-          breakpoint: 750,
-          settings: {
-            variableWidth: false,
-            autoplaySpeed: 2500,
-            slidesToShow: 4
-          }
-        }]
-      });
-    }
-
-    // ループスライダー
-    function infiniteSlider() {
-      const optionInfinite = {
-        infinite: true,
-        arrows: false,
-        swipe: false,
-        dots: false,
-        variableWidth: true,  // スライド幅の自動計算を無効
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 5000,
-        cssEase: "linear",
-        pauseOnFocus: false,
-        pauseOnHover: false
-      }
-      const slickInfinite = $('.js-infinite-slider');
-      if (slickInfinite) $(slickInfinite).slick(optionInfinite);
-    }
-
-    $(function () {
-      menuSlide();
-      owlCarousel();
-      reveal();
-      slickSlider();
-    });
+    new OwlCarousel({...this.defaultOptions});
+    new SlickSlider({...this.defaultOptions});
+    new CustomFunctions({...this.defaultOptions});
   }
 }
 
---
+++ app/assets/js/app/owl-carousel.js
@@ -0,0 +1,104 @@
+import $ from './jquery-shim.js';
+import 'owl.carousel';
+import OwlCss from "owl.carousel/dist/assets/owl.carousel.css";
+import OwlThemeCss from "owl.carousel/dist/assets/owl.theme.default.css";
+
+var defaultOptions = {
+  selector: '.owl-carousel',
+};
+
+export default class OwlCarousel {
+  /**
+   * 初期化
+   * @param options
+   */
+  constructor(options) {
+    this.options = $.extend(defaultOptions, options);
+    this.init();
+  }
+
+  /**
+   * 初期化
+   */
+  init() {
+
+    // ターゲットを取得する
+    this.targetAll = $(this.options.selector);
+
+
+    // ターゲットが存在しない場合は実行しない
+    if (!this.targetAll.length) {
+      return false;
+    }
+
+    // 実行する
+    this.run();
+  }
+
+  /**
+   * 実行する
+   */
+
+
+  run() {
+
+    // const gsap = this.options.gsap;
+
+    //->スライダー
+    this.targetAll.imagesLoaded(function () {
+      $('.c-main-visual__slider').owlCarousel({
+        items: 1,
+        margin: 0,
+        dots: true,
+        loop: true,
+        nav: false,
+        onInitialized: function () {
+          // gsap.animate
+        },
+        autoplayHoverPause: true,
+        autoplay: true,
+        autoplaySpeed: 500,
+        autoWidth: false,
+        autoHeight: false,
+        center: true,
+        animateOut: 'fadeOut',
+        navText: ['<img src="../assets/images/icon-slider-prev.svg" />', '<img src="../assets/images/icon-slider-next.svg" />'],
+      });
+
+    });
+
+
+    //->カード_カルーセル
+    this.targetAll.imagesLoaded(function () {
+      $('.js-card-slider').owlCarousel({
+        margin: 0,
+        dots: false,
+        loop: true,
+        nav: true,
+        autoplayHoverPause: true,
+        autoplay: true,
+        autoplaySpeed: 500,
+        autoWidth: false,
+        autoHeight: false,
+        center: true,
+        navText: ['<img src="../assets/images/icon-slider-prev.svg" />', '<img src="../assets/images/icon-slider-next.svg" />'],
+        responsive: {
+          // breakpoint from 0 up
+          0: {
+            items: 1,
+          },
+          // breakpoint from 750 up
+          750: {
+            items: 3,
+          }
+        }
+      });
+    });
+
+
+  }
+}
+
+
+
+
