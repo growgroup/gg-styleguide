91ba729d61ebaaccf0efcc79c2ca45885f3a21a7
akobayashi-grow-group <a.kobayashi@grow-group.jp>
2023-02-01 18:17:48 +0900

    body.en のときのfont-size指定の調整、robotoについてのコメント追加

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,6 +3,7 @@
 import "@fontsource/material-icons-outlined";
 //import "@fontsource/material-icons-sharp";
 //import "@fontsource/material-icons-two-tone";
+// 英語版作成時に本文書体で利用するためrobotoの400と700はコメントアウトしないでください
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/700.css"
 import "@fontsource/noto-sans-jp/400.css"
