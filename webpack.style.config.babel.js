5ec68b0bad206b0ef69d0195cc72b7ce71ab8f15
akobayashi-grow-group <a.kobayashi@grow-group.jp>
2023-07-18 13:45:05 +0900

    [add] css-mqpacker-webpack-plugin : webpackでCSSのメディアクエリ出力をまとめる

--- webpack.style.config.babel.js
+++ webpack.style.config.babel.js
@@ -2,6 +2,7 @@ import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import globImporter from 'node-sass-glob-importer';
 import path from 'path'
+const CssMqpackerPlugin = require('css-mqpacker-webpack-plugin');
 
 const BASE_DIR = "../../"
 
@@ -11,68 +12,75 @@ module.exports = (env, argv) => {
     const IS_DEVELOPMENT = argv.mode === 'development';
 
     const configs = {
-        mode: argv.mode,
-        context: __dirname + '/app/',
-        // cache: {
-        //     type: 'filesystem',
-        //     buildDependencies: {
-        //         config: [__filename]
-        //     }
-        // },
-        entry: {
-            style: __dirname + '/app/assets/js/style.js',
-        },
-        output: {
-            path: path.join(__dirname, 'dist/'),
-            filename: 'assets/js/[name].js',
-            publicPath: BASE_DIR,
-        },
-        watch: false,
-        module: {
-            rules: [
-                {
-                    test: /\.(scss|css)/,
-                    enforce: 'pre',
-                    loader: 'import-glob-loader'
-                },
-                {
-                    test: /\.(scss|css|sass)$/,
-                    include: [
-                        path.resolve(__dirname, 'app/assets/scss'),
-                    ],
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                url: false
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sassOptions: {
-                                    importer: globImporter()
-                                },
-                            }
-                        }
-                    ],
-                },
+      mode: argv.mode,
+      context: __dirname + '/app/',
+      // cache: {
+      //     type: 'filesystem',
+      //     buildDependencies: {
+      //         config: [__filename]
+      //     }
+      // },
+      entry: {
+        style: __dirname + '/app/assets/js/style.js',
+      },
+      output: {
+        path: path.join(__dirname, 'dist/'),
+        filename: 'assets/js/[name].js',
+        publicPath: BASE_DIR,
+      },
+      watch: false,
+      module: {
+        rules: [
+          {
+            test: /\.(scss|css)/,
+            enforce: 'pre',
+            loader: 'import-glob-loader'
+          },
+          {
+            test: /\.(scss|css|sass)$/,
+            include: [
+              path.resolve(__dirname, 'app/assets/scss'),
+            ],
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  url: false
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sassOptions: {
+                    importer: globImporter()
+                  },
+                }
+              }
+            ],
+          },
 
-            ]
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: "assets/css/[name].css",
-                chunkFilename: "[id].css"
-            })
+        ]
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: "assets/css/[name].css",
+          chunkFilename: "[id].css"
+        })
+      ],
+      optimization: {
+        //CSSのメディアクエリ部分を圧縮
+        minimize: true,
+        minimizer: [
+          new CssMqpackerPlugin(),
         ],
+      },
     }
     if (IS_DEVELOPMENT) {
-        // development であれば、devtool を追加
-        configs.devtool = 'cheap-module-source-map';
+      // development であれば、devtool を追加
+      configs.devtool = 'cheap-module-source-map';
     }
     return configs;
 }
