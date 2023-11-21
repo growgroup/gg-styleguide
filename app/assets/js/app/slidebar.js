b899901e9f1875a18a4f2fae8fb3d34ef9dff21b
増田隼也 <masudashunya@masudashunyanoMacBook-Pro.local>
2023-11-21 13:45:18 +0900

    resize point tab

--- app/assets/js/app/slidebar.js
+++ app/assets/js/app/slidebar.js
@@ -151,7 +151,7 @@ export default class Slidebar {
   setupResizeObserver() {
     const resizeObserver = new ResizeObserver(entries => {
       for (const entry of entries) {
-        if (entry.contentRect.width >= 9g50) {
+        if (entry.contentRect.width >= 950) {
           document.body.classList.remove("is-slidebar-active");
           this.isActive = false;
           this.menu.attr("inert", "true");
