426a23248acd9df2d5c0d9fb0ead2d8cf6cad3ac
akobayashi-grow-group <a.kobayashi@grow-group.jp>
2024-02-06 15:32:37 +0900

    [add] fonts.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,10 +1,6 @@
 //Material Icons
 import "../scss/font.scss";
-// 英語版作成時に本文書体で利用するためrobotoの400と700はコメントアウトしないでください
-import "@fontsource/roboto/400.css"
-import "@fontsource/roboto/700.css"
-import "@fontsource/noto-sans-jp/400.css"
-import "@fontsource/noto-sans-jp/700.css"
+
 
 import Utils from './app/utils.js';
 import Accordion from './app/accordion.js';
---
+++ app/assets/js/fonts.js
@@ -0,0 +1,5 @@
+// 英語版作成時に本文書体で利用するためrobotoの400と700はコメントアウトしないでください
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/700.css"
+import "@fontsource/noto-sans-jp/400.css"
+import "@fontsource/noto-sans-jp/700.css"
