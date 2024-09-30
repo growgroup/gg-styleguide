27c306669e906893a7f21da474b4b3043124177a
akobayashi-grow-group <a.kobayashi@grow-group.jp>
2024-09-30 19:26:24 +0900

    [update] tabs

--- app/assets/js/app/tab.js
+++ app/assets/js/app/tab.js
@@ -29,12 +29,22 @@ export default class Tab {
    * 初期化
    */
   init() {
-    this.$tabs = $(this.options.selector);
-    for (var i = 0; i < this.$tabs.length; i++) {
-      var $tab = $(this.$tabs[i]);
-      var $navs = $tab.find(this.options.navsClass).eq(0).find('*[' + this.options.navTargetAttr + ']');
-      var $firstPane = $tab.find('*[' + this.options.paneNameAttr + ']:first-of-type');
-      var $panes = $firstPane.parent().children('*[' + this.options.paneNameAttr + ']');
+    this.$tabs = $(this.options.selector); // 指定されたセレクタに該当するタブを取得
+    for (var i = 0; i < this.$tabs.length; i++) { // 各タブに対して処理を実行
+      var $tab = $(this.$tabs[i]); // 現在のタブを取得
+      var $panes = $();//jQueryオブジェクトを準備
+      var $navs = $tab.find(this.options.navsClass).eq(0).find('*[' + this.options.navTargetAttr + ']'); // ナビゲーション要素を取得
+      //ナビボタンの数だけループ
+      for (var j = 0; j < $navs.length; j++) {
+        var $nav = $($navs[j]); // 現在のナビゲーションを取得
+        var targetName = $nav.attr(this.options.navTargetAttr); // ターゲットの名前を取得
+        var $pane = $tab.find('*[' + this.options.paneNameAttr + '="' + targetName + '"]'); // ターゲットの名前と一致するパネルを取得
+        if ($pane.length === 0) { // パネルが見つからない場合
+          console.warn('Tab panel is not found.'); // エラーメッセージを出力
+        }
+        //$panesに追加ループのたびに追加
+        $panes = $panes.add($pane);
+      }
 
       if (!$navs.hasClass(this.options.activeClass)) {
         $navs.eq(0).addClass(this.options.activeClass);
