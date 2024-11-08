06927f5ac6afcde3875ac4db4d66659a5e12fbde
akobayashi-grow-group <a.kobayashi@grow-group.jp>
2024-11-08 19:41:39 +0900

    [update] move stylelint-replace-assets-path.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,11 @@
-
+const path = require('path');
 const postcssScss = require('postcss-scss');
 module.exports = {
   customSyntax: postcssScss,
-  "plugins": ["stylelint-scss","stylelint-replace-assets-path.js"],
+  "plugins": [
+    "stylelint-scss",
+    path.resolve(__dirname, "./config/stylelint-replace-assets-path.js")
+  ],
   "rules": {
     "custom/replace-assets-path": true
   }
