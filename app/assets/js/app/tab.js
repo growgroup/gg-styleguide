e7474811a2192b8c0def8b73b67ed7b7c59aac0d
onishi <onishi@onishinoMacBook-Pro.local>
2025-01-14 17:35:55 +0900

    tab.js 改善

--- app/assets/js/app/tab.js
+++ app/assets/js/app/tab.js
@@ -20,8 +20,8 @@ var defaultOptions = {
 export default class Tab {
 
   constructor(options) {
+    this.options = $.extend(true, {}, defaultOptions, options);
     this.$tabs = null;
-    this.options = $.extend(options, defaultOptions);
     this.init();
   }
 
@@ -30,39 +30,94 @@ export default class Tab {
    */
   init() {
     this.$tabs = $(this.options.selector);
-    for (var i = 0; i < this.$tabs.length; i++) {
-      var $tab = $(this.$tabs[i]);
-      var $navs = $tab.find(this.options.navsClass).eq(0).find('*[' + this.options.navTargetAttr + ']');
-      var $firstPane = $tab.find('*[' + this.options.paneNameAttr + ']:first-of-type');
-      var $panes = $firstPane.parent().children('*[' + this.options.paneNameAttr + ']');
 
-      if (!$navs.hasClass(this.options.activeClass)) {
-        $navs.eq(0).addClass(this.options.activeClass);
-      }
-      if (!$panes.hasClass(this.options.activeClass)) {
-        $panes.eq(0).addClass(this.options.activeClass);
-      }
-      this.onClick($navs, $panes);
-    }
-  }
+    this.$tabs.each((index, tab) => {
+      const $tab = $(tab);
+      const $parentNavs = $tab.find(this.options.navsClass).eq(0).find(`[${this.options.navTargetAttr}]`);
+      const $parentPanes = $tab.find(`[${this.options.paneNameAttr}]`);
 
-  /**
-   * クリック時の動作
-   */
-  onClick($navs, $panes) {
-    var self = this;
-    $navs.on('click', function(e) {
-      e.preventDefault();
-      $panes.removeClass(self.options.activeClass);
-      $navs.removeClass(self.options.activeClass);
-      $(this).addClass(self.options.activeClass);
-      var targetName = $(this).attr(self.options.navTargetAttr);
-      for (var i = 0; i < $panes.length; i++) {
-        if (targetName === $($panes[i]).attr(self.options.paneNameAttr)) {
-          $($panes[i]).addClass(self.options.activeClass);
-        }
-      }
+      // 親タブの初期設定
+      const $defaultParentNav = $parentNavs.filter(`.${this.options.activeClass}`).first() || $parentNavs.eq(0);
+      const parentTargetName = $defaultParentNav.attr(this.options.navTargetAttr);
+
+      $parentNavs.removeClass(this.options.activeClass);
+      $parentPanes.removeClass(this.options.activeClass);
+
+      $defaultParentNav.addClass(this.options.activeClass);
+      $parentPanes.filter(`[${this.options.paneNameAttr}="${parentTargetName}"]`).addClass(this.options.activeClass);
+
+      // 子タブの初期設定
+      const $childTab = $parentPanes.filter(`[${this.options.paneNameAttr}="${parentTargetName}"]`);
+      this.initChildTabs($childTab);
+
+      // 親タブクリック時の動作
+      this.onParentTabClick($parentNavs, $parentPanes);
     });
   }
 
+  /**
+   * 子タブの初期化
+   */
+  initChildTabs($childTab) {
+    const $childNavs = $childTab.find(this.options.navsClass).eq(0).find(`[${this.options.navTargetAttr}]`);
+    const $childPanes = $childTab.find(`[${this.options.paneNameAttr}]`);
+
+    const $defaultChildNav = $childNavs.filter(`.${this.options.activeClass}`).first() || $childNavs.eq(0);
+    const childTargetName = $defaultChildNav.attr(this.options.navTargetAttr);
+
+    $childNavs.removeClass(this.options.activeClass);
+    $childPanes.removeClass(this.options.activeClass);
+
+    $defaultChildNav.addClass(this.options.activeClass);
+    $childPanes.filter(`[${this.options.paneNameAttr}="${childTargetName}"]`).addClass(this.options.activeClass);
+
+    // 子タブクリック時の動作
+    this.onChildTabClick($childNavs, $childPanes);
+  }
+
+  /**
+   * 親タブクリック時の動作
+   */
+  onParentTabClick($parentNavs, $parentPanes) {
+    const self = this;
+
+    $parentNavs.on('click', function (e) {
+      e.preventDefault();
+
+      const $currentParentNav = $(this);
+      const parentTargetName = $currentParentNav.attr(self.options.navTargetAttr);
+
+      // 親タブの切り替え
+      $parentNavs.removeClass(self.options.activeClass);
+      $parentPanes.removeClass(self.options.activeClass);
+
+      $currentParentNav.addClass(self.options.activeClass);
+      const $currentParentPane = $parentPanes.filter(`[${self.options.paneNameAttr}="${parentTargetName}"]`);
+      $currentParentPane.addClass(self.options.activeClass);
+
+      // 子タブの初期化
+      self.initChildTabs($currentParentPane);
+    });
+  }
+
+  /**
+   * 子タブクリック時の動作
+   */
+  onChildTabClick($childNavs, $childPanes) {
+    const self = this;
+
+    $childNavs.on('click', function (e) {
+      e.preventDefault();
+
+      const $currentChildNav = $(this);
+      const childTargetName = $currentChildNav.attr(self.options.navTargetAttr);
+
+      // 子タブの切り替え
+      $childNavs.removeClass(self.options.activeClass);
+      $childPanes.removeClass(self.options.activeClass);
+
+      $currentChildNav.addClass(self.options.activeClass);
+      $childPanes.filter(`[${self.options.paneNameAttr}="${childTargetName}"]`).addClass(self.options.activeClass);
+    });
+  }
 }
