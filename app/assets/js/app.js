a3ed8990d1caf9fa2ad98112d7be204d0ca6d83e
akobayashi-grow-group <a.kobayashi@grow-group.jp>
2025-04-22 13:43:12 +0900

    [add] lottie-web

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -23,6 +23,7 @@ import GsapAnimation from "./app/gsap";
 import Scrollable from "./app/scrollable";
 import Dropdown from "./app/dropdown";
 import DropdownTextSync from "./app/dropdown-text-sync";
+// import LottieSimple from "./app/lottie";
 // import Parallax from "./app/parallax";
 // import LenisScroll from "./app/lenis";
 
@@ -45,6 +46,7 @@ class App {
     this.Scrollable = new Scrollable();
     // this.parallax = new Parallax();
     // this.lenis = new LenisScroll();
+    // this.LottieSimple = new LottieSimple();
     this.defaultOptions = {
       gsap: this.gsap,
       utils: this.Utils
